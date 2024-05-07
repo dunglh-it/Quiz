08cbd670841d353b93dd26b45f9b7ae2ce431fac
dunglh-it <dunglh.it@gmail.com>
2024-05-07 14:12:47 +0700

    Add HomePage.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,10 @@ const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">Demo React</Navbar.Brand>
+                {/* <Navbar.Brand href="#home">Demo React</Navbar.Brand> */}
+                <NavLink to="/" className="navbar-brand">
+                    Demo React
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -25,9 +28,9 @@ const Header = () => {
 
                     <Nav>
                         <NavDropdown title="Settings" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Log out</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+                            <NavDropdown.Item>Log in</NavDropdown.Item>
+                            <NavDropdown.Item>Log out</NavDropdown.Item>
+                            <NavDropdown.Item>Profile</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
