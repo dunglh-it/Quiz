cd27cdb2fe3571a2b234e1d3cabbfc2ac7bf46fb
dunglh-it <dunglh.it@gmail.com>
2024-05-07 09:05:01 +0700

    Add Header.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.scss"
+import Header from "./components/Header/Header"
 
 const App = () => {
-    return <div className="App">hello world</div>
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    )
 }
 
 export default App
