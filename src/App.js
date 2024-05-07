1184f834d0721b5b772513a352c5a2df02a322f8
dunglh-it <dunglh.it@gmail.com>
2024-05-07 09:33:46 +0700

    Add User.js and Admin.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.scss"
 import Header from "./components/Header/Header"
+import { Link } from "react-router-dom"
 
 const App = () => {
     return (
         <div className="App">
             <Header />
+            <div>
+                <button>
+                    <Link to="/users">go to user page</Link>
+                </button>
+                <button>
+                    <Link to="/admins">go to admin page</Link>
+                </button>
+            </div>
         </div>
     )
 }
