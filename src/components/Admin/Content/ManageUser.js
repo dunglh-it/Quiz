fe6419c051c908f2998e5393084726598a9fe811
dunglh-it <dunglh.it@gmail.com>
2024-05-09 19:02:05 +0700

    Show List Users

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -2,6 +2,7 @@ import ModalCreateUser from "./ModalCreateUser"
 import { FcPlus } from "react-icons/fc"
 import "./ManageUser.scss"
 import { useState } from "react"
+import TableUser from "./TableUser"
 
 const ManageUser = (props) => {
     const [showModalCreateUser, setShowModalCreateUser] = useState(false)
@@ -21,7 +22,9 @@ const ManageUser = (props) => {
                         <FcPlus /> Add new users
                     </button>
                 </div>
-                <div className="table-users-container">table users</div>
+                <div className="table-users-container">
+                    <TableUser />
+                </div>
                 <ModalCreateUser show={showModalCreateUser} setShow={setShowModalCreateUser} />
             </div>
         </div>
