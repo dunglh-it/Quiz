feb68c3fd31334f9c89c821d998f5c9115ba0de5
dunglh-it <dunglh.it@gmail.com>
2024-05-09 19:39:52 +0700

    Update List Users

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,11 +1,25 @@
 import ModalCreateUser from "./ModalCreateUser"
 import { FcPlus } from "react-icons/fc"
 import "./ManageUser.scss"
-import { useState } from "react"
 import TableUser from "./TableUser"
+import { useEffect, useState } from "react"
+import { getAllUsers } from "../../../services/apiService"
 
 const ManageUser = (props) => {
     const [showModalCreateUser, setShowModalCreateUser] = useState(false)
+
+    const [listUsers, setListUsers] = useState([])
+
+    useEffect(() => {
+        fetchListUsers()
+    }, [])
+
+    const fetchListUsers = async () => {
+        let res = await getAllUsers()
+        if (res.EC === 0) {
+            setListUsers(res.DT)
+        }
+    }
 
     return (
         <div className="manage-user-container">
@@ -23,9 +37,13 @@ const ManageUser = (props) => {
                     </button>
                 </div>
                 <div className="table-users-container">
-                    <TableUser />
+                    <TableUser listUsers={listUsers} />
                 </div>
-                <ModalCreateUser show={showModalCreateUser} setShow={setShowModalCreateUser} />
+                <ModalCreateUser
+                    show={showModalCreateUser}
+                    setShow={setShowModalCreateUser}
+                    fetchListUsers={fetchListUsers}
+                />
             </div>
         </div>
     )
