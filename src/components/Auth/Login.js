290853d8c0f870383050e3547cc68bb4bae6d6c3
dunglh-it <dunglh.it@gmail.com>
2024-05-11 09:46:53 +0700

    Add API Login

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,17 +1,36 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { postLogin } from "../../services/apiService"
+import { toast } from "react-toastify"
 import "./Login.scss"
 
 const Login = (props) => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const navigate = useNavigate()
 
-    const handleLogin = () => {
-        alert("login")
+    const handleLogin = async () => {
+        // validate
+
+        // submit apis
+        let data = await postLogin(email, password)
+
+        if (data && data.EC === 0) {
+            toast.success(data.EM)
+            navigate("/")
+        }
+
+        if (data && data.EC !== 0) {
+            toast.error(data.EM)
+        }
     }
 
     return (
         <div className="login-container">
-            <div className="header">Don't have an account yet?</div>
+            <div className="header">
+                <span>Don't have an account yet?</span>
+                <button>Sign up</button>
+            </div>
             <div className="title col-4 mx-auto">Demo React</div>
             <div className="welcome col-4 mx-auto">Hello, Who's this?</div>
             <div className="content-form col-4 mx-auto">
@@ -38,6 +57,17 @@ const Login = (props) => {
                     <button className="btn-submit" onClick={() => handleLogin()}>
                         Login
                     </button>
+                </div>
+
+                <div className="text-center">
+                    <span
+                        className="back"
+                        onClick={() => {
+                            navigate("/")
+                        }}
+                    >
+                        &#60;-- Go to Homepage
+                    </span>
                 </div>
             </div>
         </div>
